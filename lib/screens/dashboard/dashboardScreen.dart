eb1f857b09bcc972820af15dda12e4737b103c0b
cool-man-vk <rrvigneshkumar2002@gmail.com>
2023-05-10 19:20:26 +0530

    added sensors

--- lib/screens/dashboard/dashboardScreen.dart
+++ lib/screens/dashboard/dashboardScreen.dart
@@ -1,6 +1,7 @@
 import 'package:farmfield/widgets/dashboard/moister.dart';
+import 'package:farmfield/widgets/dashboard/sensors.dart';
 import 'package:farmfield/widgets/dashboard/weatherCard.dart';
-import 'package:farmfield/widgets/pieChart.dart';
+// import 'package:farmfield/widgets/pieChart.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -12,36 +13,74 @@ class DashBoradScreen extends StatefulWidget {
 }
 
 class _DashBoradScreenState extends State<DashBoradScreen> {
+  List navigateOptions = [
+    "Overview",
+    "Corn",
+    "Maize",
+    "Community",
+    "Profile",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       // appBar: AppBar(
       //   backgroundColor: Colors.white,
       // ),
-      backgroundColor: Color(0xFFfbfcfa),
-      body: Column(children: [
-        SizedBox(
-          height: 60,
-        ),
-        Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                "Welcome!",
-                style: GoogleFonts.robotoMono(
-                    fontSize: 25, fontWeight: FontWeight.w800),
-              ),
-              Icon(Icons.notifications_none_outlined)
-            ],
+      backgroundColor: const Color(0xFFfbfcfa),
+      body: SingleChildScrollView(
+        child: Column(children: [
+          const SizedBox(
+            height: 60,
           ),
-        ),
-        // const SizedBox(height: 20),
-        WeatherCard(),
-        // const Center(child: WeatherCard()),
-        SenorCard(),
-      ]),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Welcome!",
+                  style: GoogleFonts.robotoMono(
+                      fontSize: 25, fontWeight: FontWeight.w800),
+                ),
+                const Icon(Icons.notifications_none_outlined)
+              ],
+            ),
+          ),
+          Container(
+            height: 70,
+            margin: const EdgeInsets.all(18),
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: navigateOptions.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  padding: const EdgeInsets.all(10),
+                  width:140,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFffeceee6)),
+                    child: Text(
+                      "${navigateOptions[index]}",
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          const Sensors()
+          // const SizedBox(height: 20),
+          // WeatherCard(),
+          // const Center(child: WeatherCard()),
+          // SenorCard(),
+        ]),
+      ),
     );
   }
 }
---
+++ lib/widgets/dashboard/sensors.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+class Sensors extends StatefulWidget {
+  const Sensors({super.key});
+
+  @override
+  State<Sensors> createState() => _SensorsState();
+}
+
+class _SensorsState extends State<Sensors> {
+  final options = [
+      "Online",
+      "Low Battery",
+      "Options"
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Container(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(
+                  "Sensors",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black
+                  ),
+                ),
+                TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "All Sensors",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black
+                      ),
+                    ))
+              ],
+            ),
+            Container(
+              child: ListView.builder(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemCount: options.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    padding: const EdgeInsets.all(10),
+                    margin: const EdgeInsets.all(10),
+                    width: 180,
+                    height: 180,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xFffeceee6)),
+                      child: Text(
+                        options[index],
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
