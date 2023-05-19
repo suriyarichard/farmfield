7a8c58b03d2b15e224a1abddfeb4c40585a63509
suriyaprakash <75439259+suriyarichard@users.noreply.github.com>
2023-05-20 02:37:43 +0530

    Update mainScreen.dart

--- lib/screens/HomeScreen/mainScreen.dart
+++ lib/screens/HomeScreen/mainScreen.dart
@@ -2,10 +2,9 @@ import 'package:farmfield/pallets/color.dart';
 import 'package:farmfield/widgets/dashboard/weatherCard.dart';
 import 'package:farmfield/widgets/home/customicon.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:lottie/lottie.dart';
+import 'package:farmfield/services/final.service.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -15,132 +14,195 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  List navigateOptions = [
-    "Overview",
-    "Corn",
-    "Maize",
-    "Community",
-    "Profile",
-  ];
+  CropServiceF cropServiceF = CropServiceF();
+  List croplist = [];
+  // List navigateOptions = [
+  //   "Overview",
+  //   "Corn",
+  //   "Maize",
+  //   "Community",
+  //   "Profile",
+  // ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColor.backgroundColor,
-      body: Column(children: [
-        const SizedBox(
-          height: 50,
-        ),
-        Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                "Welcome!",
-                style: GoogleFonts.robotoMono(
-                    fontSize: 25, fontWeight: FontWeight.w800),
-              ),
-              const Icon(Icons.notifications_none_outlined)
-            ],
+      body: SingleChildScrollView(
+        child: Column(children: [
+          const SizedBox(
+            height: 50,
           ),
-        ),
-        Container(
-          height: 70,
-          margin: const EdgeInsets.all(18),
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: navigateOptions.length,
-            itemBuilder: (context, index) {
-              return Container(
-                padding: const EdgeInsets.all(10),
-                width: 140,
-                child: TextButton(
-                  onPressed: () {},
-                  style: ButtonStyle(
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(18),
-                              side: BorderSide(color: Colors.black)))),
-                  child: Text(
-                    "${navigateOptions[index]}",
-                    style: const TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
-                  ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Welcome!",
+                  style: GoogleFonts.robotoMono(
+                      fontSize: 25, fontWeight: FontWeight.w800),
                 ),
-              );
+                const Icon(Icons.notifications_none_outlined)
+              ],
+            ),
+          ),
+          FutureBuilder(
+            future: cropServiceF.get(),
+            builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                // Display a loading spinner while waiting for data
+                return Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                // Display an error message if the future throws an error
+                return Text("Error: ${snapshot.error}");
+              } else {
+                // Call the function from the instance of MyClass and display the fetched data
+                croplist = snapshot.data;
+                if (croplist.length == 0) {
+                  return Text("-");
+                  // return NoData(text: 'No CropList Available');
+                } else {
+                  // return Text(snapshot.data[0]['cropname'].toString());
+                  // return Text(
+                  // snapshot.data[index]['cropname'].toString());
+                  return Container(
+                    height: 70,
+                    margin: const EdgeInsets.all(18),
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: snapshot.data.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          padding: const EdgeInsets.all(10),
+                          width: 140,
+                          child: TextButton(
+                            onPressed: () {},
+                            style: ButtonStyle(
+                                shape: MaterialStateProperty.all<
+                                        RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(18),
+                                        side: const BorderSide(
+                                            color: Colors.black)))),
+                            child: FutureBuilder(
+                              future: cropServiceF.get(),
+                              builder: (BuildContext context,
+                                  AsyncSnapshot<dynamic> snapshot) {
+                                if (snapshot.connectionState ==
+                                    ConnectionState.waiting) {
+                                  // Display a loading spinner while waiting for data
+                                  return const Center(
+                                      child: CircularProgressIndicator());
+                                } else if (snapshot.hasError) {
+                                  // Display an error message if the future throws an error
+                                  return Text("Error: ${snapshot.error}");
+                                } else {
+                                  // Call the function from the instance of MyClass and display the fetched data
+                                  croplist = snapshot.data;
+                                  if (croplist.length == 0) {
+                                    return Text("null");
+                                    // return NoData(text: 'No CropList Available');
+                                  } else {
+                                    // return Text(snapshot.data[0]['cropname'].toString());
+                                    return Text(
+                                      snapshot.data[index]['cropname']
+                                          .toString(),
+                                      style: GoogleFonts.robotoMono(
+                                          fontSize: 16,
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.w400),
+                                    );
+                                  }
+                                }
+                              },
+                            ),
+                            // child: Text(
+                            //   "${navigateOptions[index]}",
+                            //   style: const TextStyle(
+                            //     fontSize: 16,
+                            //     fontWeight: FontWeight.bold,
+                            //     color: Colors.black,
+                            //   ),
+                            // ),
+                          ),
+                        );
+                      },
+                    ),
+                  );
+                  //
+                }
+              }
             },
           ),
-        ),
-        Lottie.network(
-            // "https://assets4.lottiefiles.com/private_files/lf30_4lyswkde.json"),
-            "https://assets4.lottiefiles.com/packages/lf20_sgn7zslb.json"),
-        // Sensors(),
-        // const SizedBox(height: 20),
-        WeatherCard(),
-        // const Center(child: WeatherCard()),
-        // SenorCard(),
-        Padding(
-          padding: const EdgeInsets.all(5.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              CustomIcon(
-                icon: const Icon(
-                  Icons.map,
-                  color: AppColor.iconColor,
-                  size: 25,
+
+          Lottie.network(
+              // "https://assets4.lottiefiles.com/private_files/lf30_4lyswkde.json"),
+              "https://assets4.lottiefiles.com/packages/lf20_sgn7zslb.json"),
+          // Sensors(),
+          // const SizedBox(height: 20),
+          WeatherCard(),
+          // const Center(child: WeatherCard()),
+          // SenorCard(),
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                CustomIcon(
+                  icon: const Icon(
+                    Icons.map,
+                    color: AppColor.iconColor,
+                    size: 25,
+                  ),
+                  text: 'Field view',
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/map');
+                  },
+                  background: AppColor.circleColor,
                 ),
-                text: 'Field view',
-                onPressed: () {
-                  Navigator.pushNamed(context, '/map');
-                },
-                background: AppColor.circleColor,
-              ),
-              CustomIcon(
-                icon: const Icon(
-                  Icons.settings_suggest,
-                  color: AppColor.iconColor,
-                  size: 25,
+                CustomIcon(
+                  icon: const Icon(
+                    Icons.settings_suggest,
+                    color: AppColor.iconColor,
+                    size: 25,
+                  ),
+                  text: 'Suggest',
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/suggestion');
+                  },
+                  background: AppColor.circleColor,
                 ),
-                text: 'Suggest',
-                onPressed: () {
-                  Navigator.pushNamed(context, '/suggestion');
-                },
-                background: AppColor.circleColor,
-              ),
-              CustomIcon(
-                icon: const Icon(
-                  Icons.crop,
-                  color: AppColor.iconColor,
-                  size: 25,
+                CustomIcon(
+                  icon: const Icon(
+                    Icons.crop,
+                    color: AppColor.iconColor,
+                    size: 25,
+                  ),
+                  text: 'Recommender',
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/');
+                  },
+                  background: AppColor.circleColor,
                 ),
-                text: 'Recommender',
-                onPressed: () {
-                  Navigator.pushNamed(context, '/');
-                },
-                background: AppColor.circleColor,
-              ),
-              CustomIcon(
-                icon: const Icon(
-                  Icons.store,
-                  color: AppColor.iconColor,
-                  size: 25,
+                CustomIcon(
+                  icon: const Icon(
+                    Icons.store,
+                    color: AppColor.iconColor,
+                    size: 25,
+                  ),
+                  text: 'Market',
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/store');
+                  },
+                  background: AppColor.circleColor,
                 ),
-                text: 'Market',
-                onPressed: () {
-                  Navigator.pushNamed(context, '/store');
-                },
-                background: AppColor.circleColor,
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-      ]),
+        ]),
+      ),
     );
   }
 }
